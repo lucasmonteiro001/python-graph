53842d84a1e0970712edfe6c71414f348db1ace6
Lucas Monteiro <lucasmonteiro001@gmail.com>
2017-06-16 09:28:17 -0300

    Run files created

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -1,4 +1,5 @@
 import unittest
+from src.ParserUtil import ParserUtil
 
 
 class ParserTestCase(unittest.TestCase):
@@ -9,9 +10,19 @@ class ParserTestCase(unittest.TestCase):
         pass
 
     def test_parses_correctly(self):
-        self.assertEquals(1, 1, "ERRADO")
 
-    def suite(self):
-        tests = ['test_parses_correctly']
+        line = ParserUtil.parse_line("AB7, TX14")
 
-        return unittest.TestSuite(map(ParserTestCase, tests))
+        edge1 = line[0]
+        edge2 = line[1]
+
+        self.assertEquals(len(line), 2, "Should have two edges")
+
+        self.assertEquals(edge1.origin, "A")
+        self.assertEquals(edge1.destination, "B")
+        self.assertEquals(edge1.distance, 7)
+
+        self.assertEquals(edge2.origin, "T")
+        self.assertEquals(edge2.destination, "X")
+        self.assertEquals(edge2.distance, 14)
+
