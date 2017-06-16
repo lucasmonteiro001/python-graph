c9d2b93d1e6e1258f713096f6bffc40a23faf3f0
Lucas Monteiro <lucasmonteiro001@gmail.com>
2017-06-16 09:35:57 -0300

    Edge tests

---
+++ tests/edge_test.py
@@ -0,0 +1,21 @@
+import unittest
+from src.Edge import Edge
+
+
+class ParserTestCase(unittest.TestCase):
+    def test_constructor(self):
+        origin = "A"
+        destination = "B"
+        distance = 7
+
+        edge = Edge(origin, destination, distance)
+
+        self.assertTrue(edge.origin == origin and edge.destination == destination and edge.distance == distance)
+
+    def test_invalid_argument(self):
+        origin = "A"
+        destination = "B"
+        distance = "7a"
+
+        with self.assertRaises(ValueError):
+            Edge(origin, destination, distance)
