dd9036725851cca1f06832a4f22a3712c3e0f68e
Lucas Monteiro <lucasmonteiro001@gmail.com>
2017-06-16 22:09:43 -0300

    refactoring

--- src/util/print_util.py
+++ src/util/print_util.py
@@ -1,11 +1,24 @@
+"""Module responsible for printing utilities"""
+
+
 class PrintUtil(object):
+    """Class that contains function to print strings to standard output"""
+
     number = 0
 
     @staticmethod
-    def print_output_test(arg, no_print=False):
+    def print_output_test(string, no_print=False):
+        """
+        Print a #string to the standard output follwing a format pattern and
+        returns the formated string
+        :param string: string to be printed
+        :param no_print: If True, don't print the string tho the standard output. Else, print it
+        :return: returns the formated string
+        """
+
         PrintUtil.number += 1
 
-        message = "Output #{}: {}".format(PrintUtil.number, str(arg))
+        message = "Output #{}: {}".format(PrintUtil.number, str(string))
 
         if not no_print:
             print message + "\n"
