c5a8741936cd536852fbc5371b8873c4f04f8047
Lucas Monteiro <lucasmonteiro001@gmail.com>
2017-06-16 23:13:48 -0300

    refactor

--- src/main.py
+++ src/main.py
@@ -1,44 +1,55 @@
+"""Main module. Runs the tests given in the documentation"""
+
 from src import PrintUtil, InputUtil, ParserUtil
+from src.graph import Graph
 
-from graph import Graph
+LINE = InputUtil.read_standard_input()
 
-line = InputUtil.read_standard_input()
-
-edges = ParserUtil.parse_string(line)
+EDGES = ParserUtil.parse_string(LINE)
 
 # Instantiates a new graph
 graph = Graph()
 
-# Add the edges to the graph
-for e in edges:
+# Add the EDGES to the graph
+for e in EDGES:
     graph.add_edge(e.origin, e.destination, e.distance)
 
 # Test #1
-PrintUtil.print_string_to_standard_output(graph.find_route_distance_among(["A", "B", "C"]))
+PrintUtil.print_string_to_standard_output(
+    graph.find_route_distance_among(["A", "B", "C"]))
 
 # Test #2
-PrintUtil.print_string_to_standard_output(graph.find_route_distance_among(["A", "D"]))
+PrintUtil.print_string_to_standard_output(
+    graph.find_route_distance_among(["A", "D"]))
 
 # Test #3
-PrintUtil.print_string_to_standard_output(graph.find_route_distance_among(["A", "D", "C"]))
+PrintUtil.print_string_to_standard_output(
+    graph.find_route_distance_among(["A", "D", "C"]))
 
 # Test #4
-PrintUtil.print_string_to_standard_output(graph.find_route_distance_among(["A", "E", "B", "C", "D"]))
+PrintUtil.print_string_to_standard_output(
+    graph.find_route_distance_among(["A", "E", "B", "C", "D"]))
 
 # Test #5
-PrintUtil.print_string_to_standard_output(graph.find_route_distance_among(["A", "E", "D"]))
+PrintUtil.print_string_to_standard_output(
+    graph.find_route_distance_among(["A", "E", "D"]))
 
 # Test #6
-PrintUtil.print_string_to_standard_output(graph.number_of_trips_starting_at_ending_at_max_stops("C", "C", 3))
+PrintUtil.print_string_to_standard_output(
+    graph.number_of_trips_starting_at_ending_at_max_stops("C", "C", 3))
 
 # Test #7
-PrintUtil.print_string_to_standard_output(graph.number_of_trips_starting_at_ending_at_exactly_stops("A", "C", 4))
+PrintUtil.print_string_to_standard_output(
+    graph.number_of_trips_starting_at_ending_at_exactly_stops("A", "C", 4))
 
 # Test #8
-PrintUtil.print_string_to_standard_output(graph.shortest_distance("A", "C"))
+PrintUtil.print_string_to_standard_output(
+    graph.shortest_distance("A", "C"))
 
 # Test #9
-PrintUtil.print_string_to_standard_output(graph.shortest_distance("B", "B"))
+PrintUtil.print_string_to_standard_output(
+    graph.shortest_distance("B", "B"))
 
 # Test #10
-PrintUtil.print_string_to_standard_output(graph.number_of_trips_starting_at_ending_at_distance_less_than("C", "C", 30))
+PrintUtil.print_string_to_standard_output(
+    graph.number_of_trips_starting_at_ending_at_distance_less_than("C", "C", 30))
